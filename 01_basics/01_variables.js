a9adb1edc0742e93bccfced92393b5dc026286dd
Ashraf-Mahmud <58129102+Ashraf-Mahmud@users.noreply.github.com>
2023-10-07 19:14:45 +0000

    learned basic variables.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accId= 123445;
+let accMail= "ashraf@gmail.com"
+var accPass= "ashrafe54"
+accCity ="Dhaka "
+
+// accId = 3
+ 
+console.log(accId);
+console.table([accId,accMail,accPass,accCity])
