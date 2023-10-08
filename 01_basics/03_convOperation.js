6f960182f70078b219da8c03f393eeaa17bbffd9
Ashraf-Mahmud <58129102+Ashraf-Mahmud@users.noreply.github.com>
2023-10-08 17:37:50 +0000

    Conversion of data types

---
+++ 01_basics/03_convOperation.js
@@ -0,0 +1,56 @@
+let score= 33; //number
+console.log(score)
+console.log(typeof score);
+
+score = Number("33");//Number is a number wrapper like in java
+console.log(score)
+console.log(typeof score); 
+
+
+score = Number("33abc");
+console.log(score)
+console.log(typeof score);// value is = not a number NaN
+
+score = Number(null);
+console.log(score)
+console.log(typeof score);// value is = 0
+
+score = Number(undefined);
+console.log(score)
+console.log(typeof score);// value is = not a number NaN
+
+score = Number(true);
+console.log(score)
+console.log(typeof score);// value is = 0/1
+
+score = Number("String");
+console.log(score)
+console.log(typeof score);// value is = not a number NaN
+
+// #############################################################################
+
+score = Boolean("String");
+console.log(score)
+console.log(typeof score);// value is = true for everything except  (undefined, null,empty string,0);
+
+score = Boolean("");
+console.log(score)
+console.log(typeof score);//
+
+score = Boolean(0);
+console.log(score)
+console.log(typeof score);//
+
+score = Boolean(null);
+console.log(score)
+console.log(typeof score);// 
+
+score = Boolean(undefined);
+console.log(score)
+console.log(typeof score);// 
+
+// ###############################################
+
+score = String(undefined);
+console.log(score)
+console.log(typeof score);// 
