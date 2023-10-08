6b853a4711d0e9b0c4c43fa12b609bda7594001d
Ashraf-Mahmud <58129102+Ashraf-Mahmud@users.noreply.github.com>
2023-10-08 17:16:27 +0000

    data types learned

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,15 @@
 "use strict"; //treat all java code as a newer version
+
+//data types
+let number=1324;
+let string="String";
+let boolean= true;
+let Null=null; //null means empty value not 0. 0 is a value in many case
+let Undefined; // undefined means value not assigned
+//unique : to define unique value like in figma or react
+
+// react 
+
+console.log(typeof "hel");
+
+//tc39.es/ecmascript   and mdn.org documentation        https://tc39.es/ecma262/
