5005a9e7260c2e73f9649a8f2395bfc42d4effd5
Ashraf-Mahmud <58129102+Ashraf-Mahmud@users.noreply.github.com>
2023-10-08 17:54:22 +0000

    conversion topic added

--- 01_basics/03_convOperation.js
+++ 01_basics/03_convOperation.js
@@ -1,56 +1,66 @@
-let score= 33; //number
-console.log(score)
-console.log(typeof score);
+// let score= 33; //number
+// console.log(score)
+// console.log(typeof score);
 
-score = Number("33");//Number is a number wrapper like in java
-console.log(score)
-console.log(typeof score); 
+// score = Number("33");//Number is a number wrapper like in java
+// console.log(score)
+// console.log(typeof score); 
 
 
-score = Number("33abc");
-console.log(score)
-console.log(typeof score);// value is = not a number NaN
+// score = Number("33abc");
+// console.log(score)
+// console.log(typeof score);// value is = not a number NaN
 
-score = Number(null);
-console.log(score)
-console.log(typeof score);// value is = 0
+// score = Number(null);
+// console.log(score)
+// console.log(typeof score);// value is = 0
 
-score = Number(undefined);
-console.log(score)
-console.log(typeof score);// value is = not a number NaN
+// score = Number(undefined);
+// console.log(score)
+// console.log(typeof score);// value is = not a number NaN
 
-score = Number(true);
-console.log(score)
-console.log(typeof score);// value is = 0/1
+// score = Number(true);
+// console.log(score)
+// console.log(typeof score);// value is = 0/1
 
-score = Number("String");
-console.log(score)
-console.log(typeof score);// value is = not a number NaN
+// score = Number("String");
+// console.log(score)
+// console.log(typeof score);// value is = not a number NaN
 
-// #############################################################################
+// // #############################################################################
 
-score = Boolean("String");
-console.log(score)
-console.log(typeof score);// value is = true for everything except  (undefined, null,empty string,0);
+// score = Boolean("String");
+// console.log(score)
+// console.log(typeof score);// value is = true for everything except  (undefined, null,empty string,0);
 
-score = Boolean("");
-console.log(score)
-console.log(typeof score);//
+// score = Boolean("");
+// console.log(score)
+// console.log(typeof score);//
 
-score = Boolean(0);
-console.log(score)
-console.log(typeof score);//
+// score = Boolean(0);
+// console.log(score)
+// console.log(typeof score);//
 
-score = Boolean(null);
-console.log(score)
-console.log(typeof score);// 
+// score = Boolean(null);
+// console.log(score)
+// console.log(typeof score);// 
 
-score = Boolean(undefined);
-console.log(score)
-console.log(typeof score);// 
+// score = Boolean(undefined);
+// console.log(score)
+// console.log(typeof score);// 
 
-// ###############################################
+// // ###############################################
 
-score = String(undefined);
-console.log(score)
-console.log(typeof score);// 
+// score = String(undefined);
+// console.log(score)
+// console.log(typeof score);// 
+
+// ############################# Operations ################################
+
+console.table([2+2,2-2,2*2,2**3,2/2,3%5]);
+
+//  ########################### Tricky conversion ################################
+
+console.table(["2"+2,1+"2",1+2+"2","2"+8+3,true,typeof true,+true,typeof +true, +"", typeof +""]);// operation continues from left to right
+
+// prefix ++8, postfix 8++ conversion mdn
